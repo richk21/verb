dccd5fb2eb9120406d57f225b989f107c63be043
richa <richa21kiran@gmail.com>
2026-09-19 22:24:26 +0530

    notificationsaga added in root

--- src/redux/rootSaga.test.ts
+++ src/redux/rootSaga.test.ts
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
-import { reportSaga } from './report/reportSaga';
 import { notificationSaga } from './notification/notificationSaga';
+import { reportSaga } from './report/reportSaga';
 import rootSaga from './rootSaga';
 import { userSaga } from './user/userSaga';
 
--- src/redux/rootSaga.ts
+++ src/redux/rootSaga.ts
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
+import { notificationSaga } from './notification/notificationSaga';
 import { reportSaga } from './report/reportSaga';
 import { userSaga } from './user/userSaga';
 
 export default function* rootSaga() {
-  yield all([userSaga(), reportSaga()]);
+  yield all([userSaga(), reportSaga(), notificationSaga()]);
 }
